630c0172e6cd634b86b319f07ec8271746d6003b
Islem <islemmanai59@gmail.com>
2026-05-02 11:42:54 +0200

    add test route

--- server.js
+++ server.js
@@ -16,3 +16,13 @@ const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
+app.post("/chatbot", async (req, res) => {
+  res.json({ reply: "test chatbot OK" });
+});
+app.get("/", (req, res) => {
+  res.send("HOME OK");
+});
+
+app.get("/test", (req, res) => {
+  res.send("TEST OK WORKING");
+});
